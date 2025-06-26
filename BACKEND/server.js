ca179ac33c735b7d08169bb54a49bc348b13e654
Mohammed Rehan <mohdrehanyahoo463@gmail.com>
2025-06-26 22:51:33 +0530

    cursorr

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -30,8 +30,11 @@ app.use((req, res, next) => {
     'http://localhost:3000'
   ];
   const origin = req.headers.origin;
+  // Always set Access-Control-Allow-Origin for allowed origins, else set to '*'
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
